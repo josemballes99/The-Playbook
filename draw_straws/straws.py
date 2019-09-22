95d06ab14249371b450146908bda2a5db0fe964f
Jose Miguel Ballesteros <ballesjm@mcmaster.ca>
2019-09-22 13:23:34 -0400

    Update straws.py

--- draw_straws/straws.py
+++ draw_straws/straws.py
@@ -1,12 +1,12 @@
 import math
 import random
 
-fam = ["Joe", "Bill", "Mike"]
-room = ["First", "Second", "Last"]
+fam = ["Joe", "Carl", "Mike"]
+spot = ["First", "Second", "Last"]
 
 for i in fam:
-    x = random.randint(0,len(room)-1)
-    print(i + " gets " + room[x])
-    room.pop(x)
+    x = random.randint(0,len(spot)-1)
+    print(i + " gets " + spot[x])
+    spot.pop(x)
 
     
